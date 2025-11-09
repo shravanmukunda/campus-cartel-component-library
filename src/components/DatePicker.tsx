311afa710357cfce0fa256fbd1e4ff978a47f881
Shravan Mukunda <shravanmukunda3@gmail.com>
2025-11-09 13:30:00 +0530

    fix: added pnpm

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { DatePicker as AntDatePicker, DatePickerProps as AntDatePickerProps } from 'antd';
+import { DatePicker as AntDatePicker } from 'antd';
+import type { DatePickerProps as AntDatePickerProps } from 'antd';
 
 export interface DatePickerProps extends AntDatePickerProps {}
 
@@ -7,13 +8,21 @@ const DatePickerWrapper: React.FC<DatePickerProps> = (props) => {
   return <AntDatePicker {...props} />;
 };
 
-export const DatePicker = Object.assign(DatePickerWrapper, {
+// Explicit type annotation
+export const DatePicker: typeof DatePickerWrapper & {
+  RangePicker: typeof AntDatePicker.RangePicker;
+  TimePicker: typeof AntDatePicker.TimePicker;
+  WeekPicker: typeof AntDatePicker.WeekPicker;
+  MonthPicker: typeof AntDatePicker.MonthPicker;
+  QuarterPicker: typeof AntDatePicker.QuarterPicker;
+  YearPicker: typeof AntDatePicker.YearPicker;
+} = Object.assign(DatePickerWrapper, {
   RangePicker: AntDatePicker.RangePicker,
   TimePicker: AntDatePicker.TimePicker,
   WeekPicker: AntDatePicker.WeekPicker,
   MonthPicker: AntDatePicker.MonthPicker,
   QuarterPicker: AntDatePicker.QuarterPicker,
   YearPicker: AntDatePicker.YearPicker,
-});
+}) as any;
 
 export default DatePicker;
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Select as AntSelect, SelectProps as AntSelectProps } from 'antd';
+import type { BaseOptionType, DefaultOptionType } from 'antd/es/select';
 
-export interface SelectProps extends AntSelectProps {}
+export interface SelectProps<
+  ValueType = any,
+  OptionType extends BaseOptionType | DefaultOptionType = DefaultOptionType
+> extends AntSelectProps<ValueType, OptionType> {}
 
-const SelectWrapper: React.FC<SelectProps> = (props) => {
-  return <AntSelect {...props} />;
+// Explicitly type the component
+const SelectWrapper = <
+  ValueType = any,
+  OptionType extends BaseOptionType | DefaultOptionType = DefaultOptionType
+>(
+  props: SelectProps<ValueType, OptionType>
+): React.ReactElement => {
+  return <AntSelect<ValueType, OptionType> {...props} />;
 };
 
-export const Select = Object.assign(SelectWrapper, {
+// Add explicit type annotation for the exported component
+export const Select: typeof SelectWrapper & {
+  Option: typeof AntSelect.Option;
+  OptGroup: typeof AntSelect.OptGroup;
+} = Object.assign(SelectWrapper, {
   Option: AntSelect.Option,
   OptGroup: AntSelect.OptGroup,
-});
+}) as any;
 
 export default Select;
